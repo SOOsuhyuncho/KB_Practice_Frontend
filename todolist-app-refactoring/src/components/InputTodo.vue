7edef08b4a5effc3bb1a3b7f526f2071982393e3
SOOsuhyuncho <chosh0132@khu.ac.kr>
2026-03-31 16:02:31 +0900

    todolist-app-refactoring by options api

---
+++ todolist-app-refactoring/src/components/InputTodo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="input-group mb-3">
+    <input
+      class="form-control"
+      @keyup="addHandler"
+      type="text"
+      v-model="todo"
+      placeholder="할일을 여기에 입력!"
+    />
+    <button type="button" class="btn btn-primary" @click="addHandler">
+      추가
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputTodo',
+  data() {
+    return {
+      todo: '',
+    };
+  },
+  methods: {
+    addHandler() {
+      this.$emit('add-todo', this.todo);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ todolist-app-refactoring/src/components/TodoListItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <li
+    class="list-group-item d-flex justify-content-between align-items-center"
+    :class="{ 'list-group-item-success': item.completed }"
+  >
+    <input
+      class="form-check-input me-2"
+      type="checkbox"
+      :checked="item.completed"
+      @change="toggleHandler"
+    />
+    <span
+      :class="{ 'text-decoration-line-through text-muted': item.completed }"
+      >{{ item.todo }}</span
+    >
+    <button class="btn btn-secondary" @click="deleteHandler">삭제</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'TodoListItem',
+  data() {
+    return {};
+  },
+  props: ['item'],
+  methods: {
+    deleteHandler() {
+      this.$emit('delete-todo', this.item.id);
+    },
+    toggleHandler() {
+      this.$emit('toggle-todo', this.item.id);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
